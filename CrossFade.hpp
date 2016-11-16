69e5e258ab3b66c70b9c40e58807c73bccef4f95
Stijn Frishert <info@stijnfrishert.com>
2016-11-16 12:05:36 +0100

    Comments for cross fade

--- CrossFade.hpp
+++ CrossFade.hpp
@@ -9,34 +9,47 @@
 #ifndef OCTOPUS_CROSS_FADE_HPP
 #define OCTOPUS_CROSS_FADE_HPP
 
+#include <dsperados/math/ease.hpp>
 #include <dsperados/math/utility.hpp>
-#include <dsperados/math/interpolation.hpp>
+#include <functional>
 
 #include "BinaryOperation.hpp"
 
 namespace octo
 {
+    //! Cross fade between two signals
     template <class T>
     class CrossFade : public BinaryOperation<T>
     {
     public:
+        //! Construct the cross fade
         CrossFade(octo::Clock& clock) :
             BinaryOperation<T>(clock),
             fade(clock)
         {
-            
+
         }
         
         GENERATE_MOVE(CrossFade)
         
     public:
+        //! The fading value (0 = left, 1 = right)
         Value<float> fade;
         
+        //! The function that will be used for easing
+        std::function<float(float)> ease = [](float v){ return math::easeLinear(v); };
+        
     private:
+        // Inherited from BinaryOperation
         void combineSamples(const T& left, const T& right, T& out) final override
         {
-            const auto phase = math::clamp<float>(fade(), 0, 1);
-            out = math::interpolateLinear(left, right, phase);
+            if (!ease)
+            {
+                out = T{};
+            } else {
+                const auto phase = math::clamp<float>(fade(), 0, 1);
+                out = ease(1 - phase) * left + ease(phase) * right;
+            }
         }
     };
 }
